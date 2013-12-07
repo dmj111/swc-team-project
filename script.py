caf9d683fd18dbeb2a2a98697ce6b507e6c9532a
Pauline Barmby <pbarmby@uwo.ca>
2013-12-06 19:07:58 -0500

    Hello world, .gitignore

--- script.py
+++ script.py
@@ -2,6 +2,9 @@ import os
 
 if __name__ == "__main__":
     # Save the world!
+    
+    # but first say hello to it!
+    print 'Hello world!'
 
     for k in xrange(5):
         with open("%d.dat" % k, "w") as fp:
